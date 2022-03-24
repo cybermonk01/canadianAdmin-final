75a4e598fe53f6e3f1def83a23d0fd27f2816905
Ashutosh Singh <75207669+cybermonk01@users.noreply.github.com>
2022-03-24 12:14:25 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,10 @@
-import path from "path";
+
 import express from "express";
 import dotenv from "dotenv";
 import colors from "colors";
 import morgan from "morgan";
 import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
+import path from "path";
 import connectDB from "./config/db.js";
 
 import productRoutes from "./routes/productRoutes.js";
